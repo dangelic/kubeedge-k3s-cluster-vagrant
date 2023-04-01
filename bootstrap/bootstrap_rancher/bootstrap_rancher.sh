2b40117377cebd3ce32787e3f2f56efc2a7eae71
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-01 12:26:42 +0200

    #feat Add bootstrap script for Rancher Server

---
+++ bootstrap/bootstrap_rancher/bootstrap_rancher.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+sudo -i
+
+curl -fsSL https://get.docker.com | sh
+
+export RANCHER_VERSION="2.7.0"
+
+sudo docker run -d --restart=unless-stopped \
+  -p 80:80 -p 8080:443 \
+  --privileged \
+  rancher/rancher:v${RANCHER_VERSION}
