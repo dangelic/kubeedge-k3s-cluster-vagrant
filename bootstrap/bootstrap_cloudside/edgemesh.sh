d631a26b197f5552c6d9b0305c2235c1bed84709
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-02 02:24:21 +0200

    #feat Add script to enable EdgeMesh

---
+++ bootstrap/bootstrap_cloudside/edgemesh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Refer: https://edgemesh.netlify.app/guide/#manual-install
+# Test: https://edgemesh.netlify.app/guide/test-case.html#cross-edge-cloud
+git clone https://github.com/kubeedge/edgemesh.git
+kubectl apply -f build/crds/istio/
+kubectl apply -f build/agent/resources/
+kubectl get all -n kubeedge -o wide
+
+# Simple Test with nginx:
+kubectl apply -f https://k8s.io/examples/application/deployment.yaml
+kubectl expose deployment nginx-deployment
+kubectl get deployments
+# curl <the CIP-Service IP>:80
