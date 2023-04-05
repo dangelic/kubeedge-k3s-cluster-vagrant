c4a6405a61811c63aec50dc5d35a506d7ece631c
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-05 21:07:38 +0200

    #refactor Disable the need to input commands during bootstrap

--- bootstrap/bootstrap_edgeside/bootstrap_edgeside.sh
+++ bootstrap/bootstrap_edgeside/bootstrap_edgeside.sh
@@ -40,8 +40,8 @@ containerd config default > /etc/containerd/config.toml
 
 # -- Setup MQTT-Client
 # Note: Keadm 1.13.0 installs MQTT by default but it does not seem to connect. A prior install will fixes this
-sudo add-apt-repository ppa:mosquitto-dev/mosquitto-ppa
-sudo apt install mosquitto mosquitto-clients
+sudo add-apt-repository -y ppa:mosquitto-dev/mosquitto-ppa
+sudo apt install -y mosquitto mosquitto-clients
 mosquitto --version
 
 # -- Setup KubeEdge in v1.13.0
