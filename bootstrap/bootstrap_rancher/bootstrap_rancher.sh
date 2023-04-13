c3e12ae1093e8b0904bf3a270e6d2b7a29353c94
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-13 20:33:59 +0200

    #refactor Kick Docker for Podman

--- bootstrap/bootstrap_rancher/bootstrap_rancher.sh
+++ bootstrap/bootstrap_rancher/bootstrap_rancher.sh
@@ -3,19 +3,32 @@
 # Disable firewall for testing
 # NOTE: A restart might be necessary
 sudo ufw disable
-
-# Get and install Docker as CRI
-# QUESTION: Is Podman possible?
-curl -fsSL https://get.docker.com | sh
+sudo apt-get update
 
 export RANCHER_VERSION="2.7.0" # Tested.
 
-# Start Rancher in container
+# Get Podman as Docker alternative
+# NOTE: Podman is not officially referenced by Rancher as valid CRI.
+# NOTE: Replace "docker" with "podman" in all related commands.
+# NOTE: Cluster spinup by Rancher leveraging Podman is not tested in this setup!
+sudo apt-get install -y podman
 
-# NOTE: In this Vagrant setup, port 8080 is forwarded to the host machine
-# Access in host machines' browser: "https://<external IP of Rancher Server specified in outline JSON>/dashboard"
-# NOTE: The UI takes some time to be accessible 
-sudo docker run -d --restart=unless-stopped \
-  -p 80:80 -p 8080:443 \
-  --privileged \
-  rancher/rancher:v${RANCHER_VERSION}
+# Add Docker.io registry to pull Rancher image
+echo -e "[registries.search]\nregistries = ['docker.io']" | sudo tee -a /etc/containers/containers.conf
+
+# Modified command
+sudo podman run -d --restart=unless-stopped \
+   -p 80:80 -p 8080:443 \
+   --privileged \
+   rancher/rancher:v${RANCHER_VERSION}
+
+# Get and install Docker as CRI
+# curl -fsSL https://get.docker.com | sh
+# Start Rancher in container
+# # NOTE: In this Vagrant setup, port 8080 is forwarded to the host machine
+# # Access in host machines' browser: "https://<external IP of Rancher Server specified in outline JSON>/dashboard"
+# # NOTE: The UI takes some time to be accessible 
+# sudo docker run -d --restart=unless-stopped \
+#   -p 80:80 -p 8080:443 \
+#   --privileged \
+#   rancher/rancher:v${RANCHER_VERSION}
