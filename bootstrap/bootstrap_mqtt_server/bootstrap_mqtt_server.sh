417efa2a03c1ec93f5ff7f1aec2822906d33f783
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-14 11:07:40 +0200

    #feat Add bootstrap script for MQTT Server

---
+++ bootstrap/bootstrap_mqtt_server/bootstrap_mqtt_server.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+MQTT_SERVER_IP=""
+
+# Parse arg
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --mqtt-server-ip=*)
+      MQTT_SERVER_IP="${1#*=}"
+      ;;
+    *)
+      echo "Error: Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# -- Setup MQTT Broker on Middleware Server
+
+sudo add-apt-repository -y ppa:mosquitto-dev/mosquitto-ppa
+sudo apt install -y mosquitto mosquitto-clients
+
+# Set bind-adress to public ip instead of localhost
+sudo echo "bind_address $MQTT_SERVER_IP" >> /etc/mosquitto/mosquitto.conf
+# Allow traffic from Clients without auth
+sudo echo "allow_anonymous true" >> /etc/mosquitto/mosquitto.conf
+sudo systemctl restart mosquitto
+
+# Add a topic for edge-to-edge (and to cloud) communication
+topic="edge-to-edge"
+mosquitto_sub -h $MQTT_SERVER_IP -p 1883 -t $topic
