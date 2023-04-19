3ffb0758e5136282cc32d87b7017c111b8a6e987
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-04-19 21:53:41 +0200

    #fix No to yes

--- ssh.sh
+++ ssh.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 sudo systemctl restart sshd
